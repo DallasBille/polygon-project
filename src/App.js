7de0f5c2ef89eb48a050c1649917b1c1fb934d5f
github username <dallasbille@gmail.com>
2019-11-01 18:02:16 -0400

    created react-mapbox-gl map

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactMapboxGl from "react-mapbox-gl";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Map = ReactMapboxGl({
+  accessToken:
+    "pk.eyJ1IjoiZGFsbGFzYmlsbGUiLCJhIjoiY2p6OHR1aGhoMDZnZDNjbXB2ZWZlcXFudCJ9.gjjYkOkTtA-Qe1jhbvF2gQ"
+});
+class App extends React.Component {
+  render() {
+    return (
+      <Map
+        center={[-73.944534, 40.6454199]}
+        style="mapbox://styles/mapbox/streets-v9"
+        containerStyle={{
+          height: "100vh",
+          width: "100vw"
+        }}
+      ></Map>
+    );
+  }
 }
-
 export default App;
